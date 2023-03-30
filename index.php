46e881e7d1c73a8d980c874db1246bfc1085e2b1
Laure <dominguezlaure@gmail.com>
2023-03-30 11:37:07 +0200

    created db and html views

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+session_start();
+
+spl_autoload_register(function ($class) {                            // $class = new Controllers\HomeController
+    require_once lcfirst(str_replace('\\', '/', $class)) . '.php';   // require_once controllers/HomeController.php
+});
+
+
+$controller = new Customers();
