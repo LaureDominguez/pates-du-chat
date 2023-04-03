72e05da57ef28141a5ed5efb5e10de2e57fda5fc
Laure <dominguezlaure@gmail.com>
2023-04-03 12:42:57 +0200

    wip css form and login

---
+++ models/Users.php
@@ -0,0 +1,52 @@
+<?php 
+
+namespace Models;
+
+use PDO;
+use PDOException;
+
+class Users extends Database {
+
+    // public function getAllCategories(){
+    //     $req = "SELECT * FROM categories";
+    //     return $this->findAll($req);
+    // }
+
+    // public function creatNew()
+    // {
+    //     $addCategory = [
+    //         'addName' => '',
+    //         'addDescript' => ''
+    //     ];
+
+    //     try {
+    //         if (array_key_exists('name', $_POST)) {
+    //             $addCategory = [
+    //                 'addName' => trim($_POST['name']),
+    //                 'addDescript' => trim($_POST['descript'])
+    //             ];
+
+    //             $req = "INSERT INTO categories (name, descript) VALUES (:name, :descript)";
+    //             $params = [
+    //                 'name', $addCategory ['addName'], PDO::PARAM_STR,
+    //                 'descript', $addCategory['addDescript'], PDO::PARAM_STR,
+    //             ];
+
+    //             var_dump($params);
+                
+    //             $this->creatOne($req, $params);
+
+    //             // $sth->bindValue('name', $addCategory['addName'], PDO::PARAM_STR);
+    //             // $sth->bindValue('descript', $addCategory['addDescript'], PDO::PARAM_STR);
+    //             // if($sth->execute()){
+    //             //     echo 'Nouvelle catégorie enregistrée';
+    //             // } else {
+    //             //     'Impossible de créer la catégorie';
+    //             // }
+    //         }
+    //     } catch (PDOException $e) {
+    //         $view = 'error';
+    //         $errors[] = 'Une erreur a eu lieu : ' . $e->getMessage();
+    //     }
+    // }
+}
