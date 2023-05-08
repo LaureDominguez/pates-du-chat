dcbcd52e5064fd6435e1e78904635aa3a1a24cee
Laure <dominguezlaure@gmail.com>
2023-05-08 21:51:00 +0200

    home animation done

--- public/js/modules/anim-reveal.js
+++ public/js/modules/anim-reveal.js
@@ -1,79 +1,42 @@
 // ********************* anim reveal on scroll
-export function revealOnScroll(){
+export function revealOnScroll() {
 
-    window.addEventListener("scroll", reveal);
-    window.addEventListener("scroll", revealR);
-    window.addEventListener("scroll", revealL);
-    reveal();
-    revealR();
-    revealL();
+    const scrollElements = document.querySelectorAll(".scroll");
 
-    let section = document.querySelectorAll("section");
-    let menu = document.querySelectorAll("#nav ul li a");
+    const elementInView = (el, dividend = 1) => {
+        const elementTop = el.getBoundingClientRect().top;
+        return (
+            elementTop <=
+            (window.innerHeight || document.documentElement.clientHeight) / dividend
+        );
+    };
 
-    window.onscroll = ()=> {
-        section.forEach(i =>{
-            let top = window.scrollY;
-            let offset = i.offsetTop -150;
-            let height = i.offsetHeight;
-            let id = i.getAttribute("id");
+    const elementOutofView = (el) => {
+        const elementTop = el.getBoundingClientRect().top;
+        return (
+            elementTop > (window.innerHeight || document.documentElement.clientHeight)
+        );
+    };
 
-            if (top >= offset && top < offset + height){
-                menu.forEach(link => {
-                    link.classList.remove("active");
-                    document.querySelector("#nav ul li a[href*=" + id + "]").classList.add("active");
-                })
+    const displayScrollElement = (element) => {
+        element.classList.add("active");
+    };
+
+    const hideScrollElement = (element) => {
+        element.classList.remove("active");
+    };
+
+    const handleScrollAnimation = () => {
+        scrollElements.forEach((el) => {
+            if (elementInView(el, 1.25)) {
+            displayScrollElement(el);
+            } else if (elementOutofView(el)) {
+            hideScrollElement(el)
             }
         })
     }
 
-    function reveal(){
-        let reveals = document.querySelectorAll(".reveal");
-        for (i = 0; i < reveals.length; i++){
-            let windowHeight = window.innerHeight;
-            let elementTop = reveals[i].getBoundingClientRect().top;
-            let elementVisible = 150;
-            if (elementTop < windowHeight - elementVisible){
-                reveals[i].classList.add("active");
-            } else {
-                reveals[i].classList.remove("active");
-            }
-        }
-    }
-
-    function revealR(){
-        let reveals = document.querySelectorAll(".revealR");
-        for (i = 0; i < reveals.length; i++){
-            let windowHeight = window.innerHeight;
-            let elementTop = reveals[i].getBoundingClientRect().top;
-            let elementVisible = 150;
-            if (elementTop < windowHeight - elementVisible){
-                reveals[i].classList.add("active");
-            } else {
-                reveals[i].classList.remove("active");
-            }
-        }
-    }
-
-    function revealL(){
-        let reveals = document.querySelectorAll(".revealL");
-        for (i = 0; i < reveals.length; i++){
-            let windowHeight = window.innerHeight;
-            let elementTop = reveals[i].getBoundingClientRect().top;
-            let elementVisible = 150;
-            if (elementTop < windowHeight - elementVisible){
-                reveals[i].classList.add("active");
-            } else {
-                reveals[i].classList.remove("active");
-            }
-        }
-    }
-
-    window.addEventListener("scroll", reveal());
-    window.addEventListener("scroll", revealR());
-    window.addEventListener("scroll", revealL());
-    reveal();
-    revealR();
-    revealL();
-
+    window.addEventListener("scroll", () => { 
+        handleScrollAnimation();
+    });
 }
