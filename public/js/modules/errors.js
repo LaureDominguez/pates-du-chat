a22934e19b90ed9d6716c83b9f52f4adf3e3334b
Laure <dominguezlaure@gmail.com>
2023-07-14 00:24:23 +0200

    made toggle for show / hide password. works good

--- public/js/modules/errors.js
+++ public/js/modules/errors.js
@@ -1,11 +1,12 @@
 // ********************* show/hide modal
 export function checkErrors() {
 
+    // register form
     const registerForm = document.querySelector('#register-form');
-    registerForm.addEventListener("submit", handleSubmit);
+    registerForm.addEventListener("submit", handleSubmitRegister);
 
-    function handleSubmit(e) {
-        e.preventDefault();
+    function handleSubmitRegister(e) {
+        e.preventDefault(); //bloque l'envoi du formulaire
 
         const data = new FormData(registerForm);
         const errorMail = document.getElementById('error_new_mail')
@@ -13,50 +14,93 @@ export function checkErrors() {
         const errorChk = document.getElementById('error_new_verif')
 
         if (data.get('email').length === 0) {
-            errorMail.innerHTML = 'entrer un email';
+            errorMail.innerHTML = 'entrer une adresse email';
+        } else {
+            errorMail.style.display = 'none';
         };
         if (data.get('pswd').length === 0) {
             errorPswd.innerHTML = 'entrer un mot de passe';
+        } else {
+            errorPswd.style.display = 'none';
         };
-        if (data.get('pswd').length > 8 && data.get('pswd_confirm').length === 0) {
+        if (data.get('pswd').length > 1 && data.get('pswd_confirm').length === 0) {
             error_new_verif.innerHTML = 'confirmer le mot de passe';
+        } else {
+            error_new_verif.style.display = 'none';
         };
 
-
-
-
-
-
-
-// let email = data.get('email');
-//         switch (email.length) {
-//             case 0:
-//                 errorMail.innerHTML = 'entrer une adresse email';
-//                 break;
-//             case 1,8:
-//                 errorMail.innerHTML = 'entrer une adresse email';
-//                 break;
-//             default:
-//                 errorMail.innerHTML = 'email inconnu';
-//         }
-
-        // for (let entry of data) {
-        //     console.log(entry);
-        //     if (entry[1] <= 2) {
-        //         console.log('renseigner le champs');
-        //     }
-        // }
-
-        // const mail = document.getElementById('email_new')
-        // const errorMail = document.getElementById('error_new_mail')
-        // const pswd = document.getElementById('pswd_new')
-        // const errorPswd = document.getElementById('error_new_pswd')
-        // const chkPswd = document.getElementById('pswd_confirm_new')
-        // const errorChk = document.getElementById('error_new_verif')
-
-        // console.log(mail);
-        // console.log(data.get('email'));
+        // data.get('pswd').setAttribute('type') === 'password';
+        // data.get('pswd_confirm').setAttribute('type') === 'password';
     }
+
+
+
+    // login form
+    const loginForm = document.querySelector('#login-form');
+    loginForm.addEventListener("submit", handleSubmitLogin);
+
+    function handleSubmitLogin(e) {
+        e.preventDefault(); //bloque l'envoi du formulaire
+
+        const data = new FormData(loginForm);
+        const errorMail = document.getElementById('error_log_mail')
+        const errorPswd = document.getElementById('error_log_pswd')
+
+        if (data.get('email').length === 0) {
+            errorMail.innerHTML = 'entrer votre adresse email';
+        } else {
+            errorMail.style.display = 'none';
+        };
+        if (data.get('pswd').length === 0) {
+            errorPswd.innerHTML = 'entrer votre mot de passe';
+        } else {
+            errorPswd.style.display = 'none';
+        };
+        
+        // data.get('pswd').setAttribute('type') === 'password';
+    }
+
+
+
+    // toggle show / hide password
+    // register form
+    const registerShow = document.getElementById('register-show')
+    const registerHide = document.getElementById('register-hide')
+    const registerPswd = document.getElementById('pswd_new');
+    const pswdConfirm = document.getElementById('pswd_confirm_new')
+    
+    // login form
+    const loginShow = document.getElementById('login-show')
+    const loginHide = document.getElementById('login-hide')
+    const loginPswd = document.getElementById('pswd_log');
+
+
+    window.addEventListener('click', function (e) {
+        switch (e.target.id) {
+            case "register-show":
+                registerShow.style.display = 'none';
+                registerHide.style.display = 'inline-flex';
+                registerPswd.setAttribute('type', 'text');
+                pswdConfirm.setAttribute('type', 'text');
+                break;
+            case "register-hide":
+                registerShow.style.display = 'inline-flex';
+                registerHide.style.display = 'none';
+                registerPswd.setAttribute('type', 'password');
+                pswdConfirm.setAttribute('type', 'password');
+                break;
+            case "login-show":
+                loginShow.style.display = 'none';
+                loginHide.style.display = 'inline-flex';
+                loginPswd.setAttribute('type', 'text');
+                break;
+            case "login-hide":
+                loginShow.style.display = 'inline-flex';
+                loginHide.style.display = 'none';
+                loginPswd.setAttribute('type', 'password');
+                break;
+        }
+        });
 
 
 
--- public/js/modules/loggin.js
+++ public/js/modules/loggin.js
@@ -7,32 +7,32 @@ export function toogleModal() {
     let closeLog = document.getElementById('close-log'); //croix de la fenetre
     let validateLog = document.getElementById('validate-log'); //bouton 'valider'
     let newAccount = document.getElementById('new-account'); //bouton 'nouveau compte'
-    let errorLogMail = document.getElementById('error_log_mail'); //msgs d'erreur
-    let errorLogPswd = document.getElementById('error_log_pswd');
+    // let errorLogMail = document.getElementById('error_log_mail'); //msgs d'erreur
+    // let errorLogPswd = document.getElementById('error_log_pswd');
 
     // register form
     let registerWindow = document.getElementById('register-window'); //fenetre de formulaire
     let closeRegister = document.getElementById('close-register'); //croix de la fenetre
     let validateRegister = document.getElementById('validate-register') //bouton 'valider'
-    let errorNewMail = document.getElementById('error_new_mail'); // msgs d'erreur
-    let errorNewPswd = document.getElementById('error_new_pswd');
-    let errorNewVerif = document.getElementById('error_new_verif');
+    // let errorNewMail = document.getElementById('error_new_mail'); // msgs d'erreur
+    // let errorNewPswd = document.getElementById('error_new_pswd');
+    // let errorNewVerif = document.getElementById('error_new_verif');
 
     // general
     let modal = document.getElementById('modal'); //div de flou sur tout l'ecran
 
 
     //s'il y a une erreur dans le formulaire de connexion, affiche la fenetre avec les erreurs
-    if(errorLogMail || errorLogPswd !== null) {
-            modal.style.display = 'block';
-            logWindow.style.display = 'flex';
-    }
+    // if(errorLogMail || errorLogPswd !== null) {
+    //         modal.style.display = 'block';
+    //         logWindow.style.display = 'flex';
+    // }
     
     //pareil avec le formulaire de connexion
-    if(errorNewMail || errorNewPswd || errorNewVerif !== null) {
-            modal.style.display = 'block';
-            registerWindow.style.display = 'flex';
-    }
+    // if(errorNewMail || errorNewPswd || errorNewVerif !== null) {
+    //         modal.style.display = 'block';
+    //         registerWindow.style.display = 'flex';
+    // }
     
     //affiche ou masque les fenetres en fonction du clic
     window.addEventListener('click', function (e) {
@@ -74,8 +74,9 @@ export function toogleModal() {
                 modal.style.display = 'none';
                 logWindow.style.display = 'none';
                 registerWindow.style.display = 'none';
-                // window.location.href = "index.php?route=clearMsg";
                 break;
         }
     })
+
+
 }
