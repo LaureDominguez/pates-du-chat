6d0570295e764a1c9a7b616e7f3af31df695b3ec
Laure <dominguezlaure@gmail.com>
2023-07-17 17:33:06 +0200

    wip

--- config/ajax.php
+++ config/ajax.php
@@ -2,55 +2,91 @@
 
 $input = json_decode(file_get_contents('php://input'), true);
 $errors = [];
-// var_dump($input);
-// var_dump($errors);
+var_dump($input);
+var_dump($errors);
 
 $email = $input['email'];
 // var_dump($email);
 
 use Models\Users;
 
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors[] = "Email ou mot de passe inconnu 1";
-        echo json_encode($errors);
-        exit;
-} else {
+if(isset($input['pswd_confirm'])){ // check for register ///////////////////////
+        var_dump('nouveau compte');
+
+        $errors = $errors_email = $errors_pswd = $errors_verif = [];
+
         $model = new Users();
-        $emailUsed = $model->checkEmail($email);
-        if (empty($emailUsed)) {
-                $errors[] = "Email ou mot de passe inconnu 2";
+        $isItFree = $model->checkEmail($email);
+        if (!empty($isItFree)){
+                $errors[] = $errors_email[] = "Cet email est déjà utilisé";
+        }
+
+        //validation mot de passe
+        $pswd = trim($input['pswd']);
+        $numberMinimal = 8;
+
+        switch ($pswd) {
+                case strlen($pswd) < $numberMinimal:
+                        $errors[] = $errors_pswd[] = "Le mot de passe doit contenir au minimum $numberMinimal caractères";
+                        break;
+                case preg_match('@[A-Z]@', $pswd) ?:
+                        $errors[] = $errors_pswd[] = "Le mot de passe doit inclure au moins une lettre majuscule";
+                        break;
+                case preg_match('@[a-z]@', $pswd) ?:
+                        $errors[] = $errors_pswd[] = "Le mot de passe doit inclure au moins une lettre minuscule";
+                        break;
+                case preg_match('@[0-9]@', $pswd) ?:
+                        $errors[] = $errors_pswd[] = "Le mot de passe doit inclure au moins un chiffre";
+                        break;
+                case preg_match('@[^\w]@', $pswd) ?:
+                        $errors[] = $errors_pswd[] = "Le mot de passe doit inclure au moins un caractère spécial";
+                        break;
+        }
+
+        $pswd_confirm = trim($input['pswd_confirm']);
+        if (empty($errors_pswd) && ($pswd != $pswd_confirm))
+        $errors[] = $errors_verif[] = "Les mots de passe ne correspondent pas";
+
+        echo json_encode($errors);
+
+} else { // check for login ////////////////////////////////
+        var_dump('connexion');
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $errors[] = "Email ou mot de passe inconnu 1";
                 echo json_encode($errors);
                 exit;
-        }
-        if (!empty($emailUsed)) {
+        } else {
                 $model = new Users();
-                $userExist = $model->getUser($emailUsed['id']);
+                $emailUsed = $model->checkEmail($email);
+                if (empty($emailUsed)) {
+                        $errors[] = "Email ou mot de passe inconnu 2";
+                        echo json_encode($errors);
+                        exit;
+                }
+                if (!empty($emailUsed)) {
+                        $model = new Users();
+                        $userExist = $model->getUser($emailUsed['id']);
 
-                // Validation du mot de passe
-
-                // Stockage du mdp pour vérification
-                $pswd = trim($input['pswd']);
-                // Si pas d'erreur, alors créer la session utilisateur. Sinon à la fin
-                if (count($errors) == 0) {
-                        if (password_verify($pswd, $userExist['pswd'])) {
-                                var_dump("c'est gagné");
-                                die;
-                                // header('Location: index.php?route=home');
-                                // exit;
+                        // Validation du mot de passe
+                        $pswd = trim($input['pswd']);
+                        // Si pas d'erreur, alors créer la session utilisateur. Sinon à la fin
+                        if (count($errors) == 0) {
+                                if (password_verify($pswd, $userExist['pswd'])) {
+                                        var_dump("c'est gagné");
+                                        die;
+                                } else {
+                                        $errors[] = "Email ou mot de passe inconnu 3";
+                                        echo json_encode($errors);
+                                        var_dump('chiant3');
+                                        exit;
+                                }
                         } else {
-                                $errors[] = "Email ou mot de passe inconnu 3";
+                                // Sinon on affiche les erreurs
                                 echo json_encode($errors);
-                                var_dump('chiant1');
+                                var_dump('chiant');
                                 exit;
                         }
-                } else {
-                        // Sinon on affiche les erreurs
-                        echo json_encode($errors);
-                        var_dump('chiant1');
-                        exit;
                 }
         }
 }
-
-// header('Location: ' . $_SESSION['visitor']['currentPage']);
-// exit;
--- public/js/modules/errors.js
+++ public/js/modules/errors.js
@@ -1,36 +1,126 @@
 // ********************* show/hide modal
+
+function isValidEmail(email) {
+  // Expression régulière pour valider le format d'une adresse e-mail
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+}
+
+// Fonction pour crypter le mot de passe côté client
+function encryptPassword(password) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(password);
+    return window.crypto.subtle.digest('SHA-256', data).then(buffer => {
+        const hexCodes = [];
+        const view = new DataView(buffer);
+        for (let i = 0; i < view.byteLength; i += 4) {
+        const value = view.getUint32(i);
+        const stringValue = value.toString(16);
+        const padding = '00000000';
+        const paddedValue = (padding + stringValue).slice(-padding.length);
+        hexCodes.push(paddedValue);
+        }
+        return hexCodes.join('');
+    });
+}
+
+//     // Exemple d'utilisation :
+//     const password = 'mon_mot_de_passe';
+//     encryptPassword(password).then(encryptedPassword => {
+//     // Utilisez l'encryptedPassword pour l'envoyer au serveur avec fetch.
+//     // Par exemple :
+//     fetch('votre_script_de_verification.php', {
+//         method: 'POST',
+//         body: JSON.stringify({ password: encryptedPassword }),
+//         headers: {
+//         'Content-Type': 'application/json'
+//         }
+//     }).then(response => {
+//         // Gérer la réponse du serveur ici.
+//     }).catch(error => {
+//         console.error('Erreur lors de l\'envoi du mot de passe :', error);
+//     });
+// });
+
+
 export function checkErrors() {
 
-    // register form
+    // register form ///////////////////
     const registerForm = document.querySelector('#register-form');
     registerForm.addEventListener("submit", handleSubmitRegister);
 
     function handleSubmitRegister(e) {
         e.preventDefault(); //bloque l'envoi du formulaire
 
-        const data = new FormData(registerForm);
+        const newData = new FormData(registerForm);
         const errorMail = document.getElementById('error_new_mail')
         const errorPswd = document.getElementById('error_new_pswd')
         const errorChk = document.getElementById('error_new_verif')
+        const email = newData.get('email');
+        const pswd = newData.get('pswd')
 
-        if (data.get('email').length === 0) {
+        if (!email) {
             errorMail.innerHTML = 'entrer une adresse email';
-        } else {
-            errorMail.style.display = 'none';
-        };
-        if (data.get('pswd').length === 0) {
+            return;
+        }
+        if (!isValidEmail(email)) {
+            errorMail.innerHTML = 'entrer une adresse email valide';
+            return;
+        }
+        errorMail.style.display = 'none';
+
+
+        if (!newData.get('pswd')) {
             errorPswd.innerHTML = 'entrer un mot de passe';
         } else {
             errorPswd.style.display = 'none';
         };
-        if (data.get('pswd').length > 1 && data.get('pswd_confirm').length === 0) {
+        if (newData.get('pswd').length > 1 && !newData.get('pswd_confirm')) {
             error_new_verif.innerHTML = 'confirmer le mot de passe';
         } else {
             error_new_verif.style.display = 'none';
         };
 
-        // data.get('pswd').setAttribute('type') === 'password';
-        // data.get('pswd_confirm').setAttribute('type') === 'password';
+        console.log('renvois les erreurs ?')
+        
+        
+        
+        const jsonData = JSON.stringify(Object.fromEntries(newData))
+        console.log(jsonData);
+
+        fetch('./config/ajax.php', {
+        method: 'POST',
+        body: jsonData
+    })
+        .then(response => {
+    if (!response.ok) {
+        throw new Error('Erreur de requête : ' + response.status);
+        }
+        return response.text();
+    })
+    .then(errors => {
+        // Traitez les données de réponse ici
+        if (errors.length > 0) {
+            console.log('liste des erreurs register :')
+            console.log(errors);
+            console.log('fin de liste');
+        }
+        else {
+            console.log('pas d\'erreur');
+            // window.location.href = 'index.php?route=connect';
+            loginForm.submit();
+        }
+        console.log("pouet 1");
+
+        
+    })
+    .catch(error => {
+        // Traitez les erreurs de la requête Fetch
+        console.error('Erreur de requête Fetch :', error);
+    });
+
+        // newData.get('pswd').setAttribute('type') === 'password';
+        // newData.get('pswd_confirm').setAttribute('type') === 'password';
         // if ("pas d'errreurs") {
         //     loginForm.submit();
         // }
@@ -79,15 +169,16 @@ export function checkErrors() {
     .then(errors => {
         // Traitez les données de réponse ici
         if (errors.length > 0) {
-            console.log('liste des erreurs :')
+            console.log('liste des erreurs loggin :')
             console.log(errors);
-            console.log('fin del iste');
+            console.log('fin de liste');
         }
         else {
             console.log('pas d\'erreur');
-            window.location.href = 'index.php?route=connect';
+            // window.location.href = 'index.php?route=connect';
+            loginForm.submit();
         }
-        console.log("pouet");
+        console.log("pouet2");
 
         
     })
