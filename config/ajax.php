957a00aa68a5062145976124b01a4375848ad9f5
Laure <dominguezlaure@gmail.com>
2023-07-17 21:43:44 +0200

    cannot reach db

--- config/ajax.php
+++ config/ajax.php
@@ -1,92 +1,83 @@
 <?php
 
+use Models\Users;
+
+// require_once '../models/Users.php';
+// require_once '../models/Database.php';
+
 $input = json_decode(file_get_contents('php://input'), true);
 $errors = [];
-var_dump($input);
-var_dump($errors);
-
 $email = $input['email'];
-// var_dump($email);
-
-use Models\Users;
 
 if(isset($input['pswd_confirm'])){ // check for register ///////////////////////
         var_dump('nouveau compte');
 
-        $errors = $errors_email = $errors_pswd = $errors_verif = [];
-
         $model = new Users();
         $isItFree = $model->checkEmail($email);
         if (!empty($isItFree)){
-                $errors[] = $errors_email[] = "Cet email est déjà utilisé";
+                $errors[] = "Cet email est déjà utilisé";
+                return json_encode($errors);
         }
 
         //validation mot de passe
         $pswd = trim($input['pswd']);
         $numberMinimal = 8;
 
-        switch ($pswd) {
-                case strlen($pswd) < $numberMinimal:
-                        $errors[] = $errors_pswd[] = "Le mot de passe doit contenir au minimum $numberMinimal caractères";
-                        break;
-                case preg_match('@[A-Z]@', $pswd) ?:
-                        $errors[] = $errors_pswd[] = "Le mot de passe doit inclure au moins une lettre majuscule";
-                        break;
-                case preg_match('@[a-z]@', $pswd) ?:
-                        $errors[] = $errors_pswd[] = "Le mot de passe doit inclure au moins une lettre minuscule";
-                        break;
-                case preg_match('@[0-9]@', $pswd) ?:
-                        $errors[] = $errors_pswd[] = "Le mot de passe doit inclure au moins un chiffre";
-                        break;
-                case preg_match('@[^\w]@', $pswd) ?:
-                        $errors[] = $errors_pswd[] = "Le mot de passe doit inclure au moins un caractère spécial";
-                        break;
+        if (strlen($pswd) < $numberMinimal) {
+                $errors[] = "Le mot de passe doit contenir au minimum $numberMinimal caractères";
+                return json_encode($errors);
+        }
+
+        if (!preg_match('@[A-Z]@', $pswd)) {
+                $errors[] = "Le mot de passe doit inclure au moins une lettre majuscule";
+                return json_encode($errors);
+        }
+
+        if (!preg_match('@[a-z]@', $pswd)) {
+                $errors[] = "Le mot de passe doit inclure au moins une lettre minuscule";
+                return json_encode($errors);
+        }
+
+        if (!preg_match('@[0-9]@', $pswd)) {
+                $errors[] = "Le mot de passe doit inclure au moins un chiffre";
+                return json_encode($errors);
+        }
+
+        if (!preg_match('@[^\w]@', $pswd)) {
+                $errors[] = "Le mot de passe doit inclure au moins un caractère spécial";
+                return json_encode($errors);
         }
 
         $pswd_confirm = trim($input['pswd_confirm']);
-        if (empty($errors_pswd) && ($pswd != $pswd_confirm))
-        $errors[] = $errors_verif[] = "Les mots de passe ne correspondent pas";
+        
+        if (empty($errors) && ($pswd != $pswd_confirm)) {
+                $errors[] = "Les mots de passe ne correspondent pas";
+                return json_encode($errors);
+        }
 
-        echo json_encode($errors);
+        // if (!empty($errors)) {
+        //         echo json_encode($errors);
+        // }
 
 } else { // check for login ////////////////////////////////
         var_dump('connexion');
 
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $models = new Users();
+        $emailUsed = $model->checkEmail($email);
+        if (empty($emailUsed)) {
                 $errors[] = "Email ou mot de passe inconnu 1";
-                echo json_encode($errors);
+                return json_encode($errors);
+        }
+
+        if (!empty($emailUsed) && password_verify($pswd, $userExist['pswd'])) {
+                var_dump("c'est gagné");
                 exit;
         } else {
-                $model = new Users();
-                $emailUsed = $model->checkEmail($email);
-                if (empty($emailUsed)) {
-                        $errors[] = "Email ou mot de passe inconnu 2";
-                        echo json_encode($errors);
-                        exit;
-                }
-                if (!empty($emailUsed)) {
-                        $model = new Users();
-                        $userExist = $model->getUser($emailUsed['id']);
-
-                        // Validation du mot de passe
-                        $pswd = trim($input['pswd']);
-                        // Si pas d'erreur, alors créer la session utilisateur. Sinon à la fin
-                        if (count($errors) == 0) {
-                                if (password_verify($pswd, $userExist['pswd'])) {
-                                        var_dump("c'est gagné");
-                                        die;
-                                } else {
-                                        $errors[] = "Email ou mot de passe inconnu 3";
-                                        echo json_encode($errors);
-                                        var_dump('chiant3');
-                                        exit;
-                                }
-                        } else {
-                                // Sinon on affiche les erreurs
-                                echo json_encode($errors);
-                                var_dump('chiant');
-                                exit;
-                        }
-                }
+                $errors[] = "Email ou mot de passe inconnu 2";
+                return json_encode($errors);
         }
+
+        // if (!empty($errors)) {
+        //         echo json_encode($errors);
+        // }
 }
--- public/js/modules/errors.js
+++ public/js/modules/errors.js
@@ -1,5 +1,156 @@
 // ********************* show/hide modal
 
+export function checkErrors() {
+
+    // register form ///////////////////
+
+    const registerForm = document.querySelector('#register-form');
+    registerForm.addEventListener("submit", handleSubmitRegister);
+
+    function handleSubmitRegister(e) {
+        e.preventDefault(); //bloque l'envoi du formulaire
+
+        const newData = new FormData(registerForm);
+        const email = newData.get('email');
+        const pswd = newData.get('pswd');
+        const pswd_confirm = newData.get('pswd_confirm');
+        const errorMail = document.getElementById('error_new_mail')
+        const errorPswd = document.getElementById('error_new_pswd')
+        const errorChk = document.getElementById('error_new_verif')
+        let errors = false;
+
+        if (!email) {
+            errorMail.innerHTML = 'entrer une adresse email';
+            errors = true;
+            return;
+        }
+        if (!isValidEmail(email)) {
+            errorMail.innerHTML = 'entrer une adresse email valide';
+            errors = true;
+            return;
+        }
+        errorMail.style.display = 'none';
+
+
+        if (!pswd) {
+            errorPswd.innerHTML = 'entrer un mot de passe';
+            errors = true;
+            return;
+        }
+        errorPswd.style.display = 'none';
+
+        if (pswd.length > 1 && !pswd_confirm) {
+            error_new_verif.innerHTML = 'confirmer le mot de passe';
+            errors = true;
+            return;
+        }
+        error_new_verif.style.display = 'none';
+
+
+        // console.error(error);
+        console.log('renvois les erreurs ?')
+        if (errors == false) {
+            console.log('squalala');
+            fetchData(newData);
+            return;
+        }
+
+        // if ("pas d'errreurs") {
+        //     registerForm.submit();
+        // }
+    }
+
+////////////////////////////////////////////////////////////////////
+
+    // login form
+
+    const loginForm = document.querySelector('#login-form');
+    loginForm.addEventListener("submit", handleSubmitLogin);
+
+    function handleSubmitLogin(e) {
+        e.preventDefault(); //bloque l'envoi du formulaire
+
+        const logData = new FormData(loginForm);
+        const errorMail = document.getElementById('error_log_mail')
+        const errorPswd = document.getElementById('error_log_pswd')
+        const email = logData.get('email');
+        let errors = false;
+
+        if (!email) {
+            errorMail.innerHTML = 'entrer une adresse email';
+            errors = true;
+            return;
+        }
+        if (!isValidEmail(email)) {
+            errorMail.innerHTML = 'entrer une adresse email valide';
+            errors = true;
+            return;
+        }
+        errorMail.style.display = 'none'
+
+        console.log(errors);
+
+        if (!logData.get('pswd')) {
+            errorPswd.innerHTML = 'entrer votre mot de passe';
+            errors = true;
+            return;
+        }
+        errorPswd.style.display = 'none';
+
+        if (errors === false) {
+            console.log('nous sommes partis');
+            fetchData(logData);
+            return;
+        }
+        
+        // if ("pas d'errreurs") {
+        //     loginForm.submit();
+        // }
+    }
+
+
+
+    // button toggle show/hide password :
+    // register form
+    const registerShow = document.getElementById('register-show')
+    const registerHide = document.getElementById('register-hide')
+    const registerPswd = document.getElementById('pswd_new');
+    const pswdConfirm = document.getElementById('pswd_confirm_new')
+
+    // login form
+    const loginShow = document.getElementById('login-show')
+    const loginHide = document.getElementById('login-hide')
+    const loginPswd = document.getElementById('pswd_log');
+
+
+    window.addEventListener('click', function (e) {
+        switch (e.target.id) {
+            case "register-show":
+                registerShow.style.display = 'none';
+                registerHide.style.display = 'inline-flex';
+                registerPswd.setAttribute('type', 'text');
+                pswdConfirm.setAttribute('type', 'text');
+                break;
+            case "register-hide":
+                registerShow.style.display = 'inline-flex';
+                registerHide.style.display = 'none';
+                registerPswd.setAttribute('type', 'password');
+                pswdConfirm.setAttribute('type', 'password');
+                break;
+            case "login-show":
+                loginShow.style.display = 'none';
+                loginHide.style.display = 'inline-flex';
+                loginPswd.setAttribute('type', 'text');
+                break;
+            case "login-hide":
+                loginShow.style.display = 'inline-flex';
+                loginHide.style.display = 'none';
+                loginPswd.setAttribute('type', 'password');
+                break;
+        }
+    });
+}
+
 function isValidEmail(email) {
   // Expression régulière pour valider le format d'une adresse e-mail
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -42,53 +193,11 @@ function encryptPassword(password) {
 //     });
 // });
 
+function fetchData(data) {
+    const jsonData = JSON.stringify(Object.fromEntries(data))
+    // console.log(jsonData);
 
-export function checkErrors() {
-
-    // register form ///////////////////
-    const registerForm = document.querySelector('#register-form');
-    registerForm.addEventListener("submit", handleSubmitRegister);
-
-    function handleSubmitRegister(e) {
-        e.preventDefault(); //bloque l'envoi du formulaire
-
-        const newData = new FormData(registerForm);
-        const errorMail = document.getElementById('error_new_mail')
-        const errorPswd = document.getElementById('error_new_pswd')
-        const errorChk = document.getElementById('error_new_verif')
-        const email = newData.get('email');
-        const pswd = newData.get('pswd')
-
-        if (!email) {
-            errorMail.innerHTML = 'entrer une adresse email';
-            return;
-        }
-        if (!isValidEmail(email)) {
-            errorMail.innerHTML = 'entrer une adresse email valide';
-            return;
-        }
-        errorMail.style.display = 'none';
-
-
-        if (!newData.get('pswd')) {
-            errorPswd.innerHTML = 'entrer un mot de passe';
-        } else {
-            errorPswd.style.display = 'none';
-        };
-        if (newData.get('pswd').length > 1 && !newData.get('pswd_confirm')) {
-            error_new_verif.innerHTML = 'confirmer le mot de passe';
-        } else {
-            error_new_verif.style.display = 'none';
-        };
-
-        console.log('renvois les erreurs ?')
-        
-        
-        
-        const jsonData = JSON.stringify(Object.fromEntries(newData))
-        console.log(jsonData);
-
-        fetch('./config/ajax.php', {
+    fetch('./config/ajax.php', {
         method: 'POST',
         body: jsonData
     })
@@ -106,161 +215,16 @@ export function checkErrors() {
             console.log('fin de liste');
         }
         else {
-            console.log('pas d\'erreur');
+            console.log('ca passe');
             // window.location.href = 'index.php?route=connect';
             loginForm.submit();
         }
         console.log("pouet 1");
-
         
     })
     .catch(error => {
         // Traitez les erreurs de la requête Fetch
         console.error('Erreur de requête Fetch :', error);
-    });
-
-        // newData.get('pswd').setAttribute('type') === 'password';
-        // newData.get('pswd_confirm').setAttribute('type') === 'password';
-        // if ("pas d'errreurs") {
-        //     loginForm.submit();
-        // }
-    }
-
-////////////////////////////////////////////////////////////////////
-
-    // login form
-
-    const loginForm = document.querySelector('#login-form');
-    loginForm.addEventListener("submit", handleSubmitLogin);
-
-    function handleSubmitLogin(e) {
-        e.preventDefault(); //bloque l'envoi du formulaire
-
-        const formData = new FormData(loginForm);
-        const errorMail = document.getElementById('error_log_mail')
-        const errorPswd = document.getElementById('error_log_pswd')
-
-        if (!formData.get('email')) {
-            return errorMail.innerHTML = 'entrer votre adresse email';
-        } else {
-            errorMail.style.display = 'none';
-        };
-        if (!formData.get('pswd')) {
-            return errorPswd.innerHTML = 'entrer votre mot de passe';
-        } else {
-            errorPswd.style.display = 'none';
-        };
-        
-        const jsonData = JSON.stringify(Object.fromEntries(formData))
-        console.log(jsonData);
-
-    // Envoyez la requête POST au fichier PHP
-    // fetch('./controllers/UsersController.php', {
-    fetch('./config/ajax.php', {
-        method: 'POST',
-        body: jsonData
-    })
-        .then(response => {
-    if (!response.ok) {
-        throw new Error('Erreur de requête : ' + response.status);
-        }
-        return response.text();
-    })
-    .then(errors => {
-        // Traitez les données de réponse ici
-        if (errors.length > 0) {
-            console.log('liste des erreurs loggin :')
-            console.log(errors);
-            console.log('fin de liste');
-        }
-        else {
-            console.log('pas d\'erreur');
-            // window.location.href = 'index.php?route=connect';
-            loginForm.submit();
-        }
-        console.log("pouet2");
-
-        
-    })
-    .catch(error => {
-        // Traitez les erreurs de la requête Fetch
-        console.error('Erreur de requête Fetch :', error);
-    });
-        
-        
-        // errorMail.innerHTML = data;
-    // .then(response => response.blob())
-    // .then(errors => {
-    //     // Traitez les erreurs renvoyées
-    //     console.log(Object);
-    //     if (Object.keys(errors).length > 0) {
-    //     // Affichez les erreurs à l'utilisateur
-    //     for (let field in errors) {
-    //         // Exemple : Afficher les erreurs à côté des champs correspondants
-    //         console.log('ya des erreurs');
-    //         // const errorContainer = document.getElementById(`${field}-error`);
-    //         // errorContainer.textContent = errors[field];
-    //         console.log(field)
-    //     }
-    //     } else {
-    //     // Les données du formulaire sont valides, effectuez l'action souhaitée
-    //     // Exemple : Redirigez l'utilisateur vers une autre page
-    //     // window.location.href = 'succes.html';
-    //         console.log('succès ?')
-    //     }
-    // })
-    // .catch(error => {
-    //     // Traitez les erreurs de la requête Fetch
-    //     console.error('Erreur de requête Fetch :', error);
-    // });
-
-
-        
-    //     data.get('pswd').setAttribute('type') === 'password';
-    //     if ("pas d'errreurs") {
-    //         loginForm.submit();
-    //     }
-    }
-
-
-
-    // toggle show / hide password
-    // register form
-    const registerShow = document.getElementById('register-show')
-    const registerHide = document.getElementById('register-hide')
-    const registerPswd = document.getElementById('pswd_new');
-    const pswdConfirm = document.getElementById('pswd_confirm_new')
-
-    // login form
-    const loginShow = document.getElementById('login-show')
-    const loginHide = document.getElementById('login-hide')
-    const loginPswd = document.getElementById('pswd_log');
-
-
-    window.addEventListener('click', function (e) {
-        switch (e.target.id) {
-            case "register-show":
-                registerShow.style.display = 'none';
-                registerHide.style.display = 'inline-flex';
-                registerPswd.setAttribute('type', 'text');
-                pswdConfirm.setAttribute('type', 'text');
-                break;
-            case "register-hide":
-                registerShow.style.display = 'inline-flex';
-                registerHide.style.display = 'none';
-                registerPswd.setAttribute('type', 'password');
-                pswdConfirm.setAttribute('type', 'password');
-                break;
-            case "login-show":
-                loginShow.style.display = 'none';
-                loginHide.style.display = 'inline-flex';
-                loginPswd.setAttribute('type', 'text');
-                break;
-            case "login-hide":
-                loginShow.style.display = 'inline-flex';
-                loginHide.style.display = 'none';
-                loginPswd.setAttribute('type', 'password');
-                break;
-        }
     });
 }
+
