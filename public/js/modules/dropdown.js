2cdc765a72808ebccee2993831dfdfb95a96547f
Laure <dominguezlaure@gmail.com>
2023-07-27 02:02:03 +0200

    made a lot of css... made most of the media query, made a new nav with burger button for mobile screan, and another homepage for mobile on landscape side... almost everything looks fine, but the account dropdown menu is broke... need to fix it tomorrow

--- public/js/modules/dropdown.js
+++ public/js/modules/dropdown.js
@@ -5,6 +5,7 @@ export function dropdownNav() {
     let menu = document.getElementById('dropdown-menu');
 
     window.addEventListener('click', function (e) {
+        console.log('pouet')
         if (dropdown !== null) {
             if (dropdown.contains(e.target)) {
                 menu.style.display = "block";
@@ -14,4 +15,22 @@ export function dropdownNav() {
             }
         }
     })
+
+    const burger = document.getElementById('burger');
+    const menuMobile = document.getElementById('menu-mobile');
+    if(burger) {
+        burger.addEventListener('click', function (e) {
+            switch (menuMobile.style.opacity) {
+                case '0':
+                    menuMobile.style.opacity = '1';
+                    break;
+                case '1':
+                    menuMobile.style.opacity = '0';
+                    break;
+                default:
+                    menuMobile.style.opacity = '1';
+                    break;
+            }
+        })
+    }
 }
